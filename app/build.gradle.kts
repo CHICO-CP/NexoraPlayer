b4f68d9eae3afde0a779837d0a7d128953ba1129
Auto Commit <autocommit@example.com>
2026-06-27 18:14:01 -0700

    fix: builconfig [2026-06-27 18:14:01]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 5
-        versionName = "0.5.7"
+        versionName = "0.5.5"
     }
 
     val releaseKeystorePath = System.getenv("KEYSTORE_FILE") ?: "app/ghostnexora-release.jks"
@@ -41,6 +41,7 @@ android {
 
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 
     composeOptions {
